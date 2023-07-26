b14d2a000981470bf888d01ab5f9e400af21ff1d
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-26 23:25:42 +0530

    third

--- components/index.js
+++ components/index.js
@@ -1,3 +1,6 @@
 import PriceAlert from '../components/PriceAlert';
+import TransactionHistory from './TransactionHistory';
+import HeaderBar from './HeaderBar';
+import CurrencyLabel from './CurrencyLabel';
 
-export { PriceAlert} ;
+export { PriceAlert, TransactionHistory, HeaderBar, CurrencyLabel} ;
