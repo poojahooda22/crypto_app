046773755e66447268161caac3992b9c43cb5ff7
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-10 22:47:41 +0530

    first

--- App.js
+++ App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { CryptoDetail, Transaction } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
-import SplashScreen from 'react-native-splash-screen';
-
+// import SplashScreen from 'react-native-splash-screen';
+import { useFonts } from 'expo-font';
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
 
-  React.useEffect(() => {
-    SplashScreen.hide();
-  }, [])
+  // React.useEffect(() => {
+  //   SplashScreen.hide();
+  // }, [])
+
+
+  const [loaded] =useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+
+  });
+
+  if(!loaded) return null;
+  
   return (
     <NavigationContainer>
       <Stack.Navigator
